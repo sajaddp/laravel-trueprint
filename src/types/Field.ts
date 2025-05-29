f6855fa6bc93c12e82d755955d77d2f93062b08c
Sajad Dehshiri <sajaddp71@gmail.com>
2025-05-30 00:10:39 +0330

    Update Field.ts

--- src/types/Field.ts
+++ src/types/Field.ts
@@ -7,4 +7,30 @@ export interface Field extends TypeSpecificArgs, ColumnModifiers {
   type: FieldType;
   nullable?: boolean;
   default?: string | number | boolean | null;
+
+  // Indexes
+  primary?: boolean;
+  unique?: boolean;
+  index?: boolean;
+  fullText?: boolean;
+  spatialIndex?: boolean;
+  indexName?: string;
+
+  // Foreign key constraints
+  constrained?: boolean;
+  constrainedTable?: string;
+  constrainedIndexName?: string;
+  onDelete?: "cascade" | "restrict" | "set null" | "no action";
+  onUpdate?: "cascade" | "restrict" | "set null" | "no action";
+
+  // Expressive FK helpers
+  cascadeOnDelete?: boolean;
+  restrictOnDelete?: boolean;
+  nullOnDelete?: boolean;
+  noActionOnDelete?: boolean;
+
+  cascadeOnUpdate?: boolean;
+  restrictOnUpdate?: boolean;
+  nullOnUpdate?: boolean;
+  noActionOnUpdate?: boolean;
 }
